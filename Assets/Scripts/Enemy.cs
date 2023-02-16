c9201acf180c546340cedbbf2025c74ff5466d85
Leo Kenny <94982692+Faoladh@users.noreply.github.com>
2023-02-16 17:36:14 +0000

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
+
+    [SerializeField] Transform parent;
+
+    [SerializeField] int scorePerHit;
+    [SerializeField] int hitPoints = 4;
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidbody();
+    }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+
+        if(hitPoints <= 0)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints --;
+    }
+
+    void KillEnemy()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("General Settings")]
+    [Tooltip("How fast the ship flies up and down")] [SerializeField] float controlSpeed = 30f;
+    [Tooltip("Adjusts the width of the ship can fly so it doesnt fly off screen")] [SerializeField] float xRange = 12f;
+    [Tooltip("Adjusts the height of the ship can fly so it doesnt fly off screen")] [SerializeField] float yRange = 7f;
+
+    [Header("Laser Gun Array")]
+    [SerializeField] GameObject[] lasers;
+
+    [Header("Screen position based tuning")]
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = 3f;
+
+    [Header("Player input based tuning")]
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow;
+    float yThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+        ProcessFiring();
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessFiring()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            SetLasersActive(true);
+        }
+        else
+        {
+            SetLasersActive(false);
+        }
+    }
+
+    void SetLasersActive(bool isActive)
+    {
+        foreach(GameObject laser in lasers)
+        {
+            var emmisionModule = laser.GetComponent<ParticleSystem>().emission;
+
+            emmisionModule.enabled = isActive;
+        }
+    }
+
+}
